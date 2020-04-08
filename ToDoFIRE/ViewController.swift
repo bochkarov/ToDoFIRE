43e6aa1683c2aa809abe636d30464bdce35a3b0c
Valentyn Bochkarov <bochkarov90@gmail.com>
2020-04-08 21:45:16 +0200

    Initial Commit

---
+++ ToDoFIRE/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoFIRE
+//
+//  Created by Bochkarov Valentyn on 08/04/2020.
+//  Copyright © 2020 Bochkarov Valentyn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
